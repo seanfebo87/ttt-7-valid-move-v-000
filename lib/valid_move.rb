419669aad0d5dfa567a78a4c56819051e4485c0e
Sean Febo <seanfebo@techie.com>
2018-04-03 08:43:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,12 @@
 def valid_move?(board, index)
-
+  if index.between(0, 8) 
+    return true
+  elsif index == " "
+    return true
+  else
+    return false
+end
+end
 
 
 def position_taken?(board, index)
